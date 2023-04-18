8a1c5d5b583b1cc63c3964f2188f8db3da0be41b
Manuel Espinosa <manuel.espinosa@live.com>
2023-04-18 10:08:03 -0600

    Add new line

--- fortune.sh
+++ fortune.sh
@@ -6,7 +6,7 @@ then
 else
 	cows=($(find /usr/share/cowsay/cows -name *.cow -printf "%f\n"))
 	cow_number=$(($RANDOM % ${#cows[@]} + 1))
-	printf "${cows[$cow_number]}" | sed 's/.cow$/ says: \n/' | lolcat -F 0.75
+	printf "\n${cows[$cow_number]}" | sed 's/.cow$/ says: \n/' | lolcat -F 0.75
 	sleep 1
 	COWFILE=$(echo "-f ${cows[$cow_number]}" | sed 's/.cow$//')
 fi
@@ -16,7 +16,7 @@ fi
 printf '\n' >> fortune.txt
 
 fortune | tee -a fortune.txt | \
-       (cowsay $COWFILE && printf "\n") | \
+       (printf "\n" && cowsay $COWFILE && printf "\n") | \
        lolcat -a -F 0.5 -s 40 -p $(($RANDOM % 10 + 1)) && \
        printf '_%.0s' {1..40} >> fortune.txt && \
        printf '\n' >> fortune.txt
