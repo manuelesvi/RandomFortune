791be763603d3363335fda00d62ce7c887e5fc14
Manuel Espinosa <manuel.espinosa@live.com>
2024-06-23 15:27:38 -0600

    comments & remove error at line 33

--- fortune.sh
+++ fortune.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
 
-COWFILE=""
+# Este script obtiene frase cualquiera del comando fortune
+# e imprime en pantalla con los colores del arcoiris
+# la frase.
+
+COWFILE="" # el nombre de la vaca
+
 if [ $# -ge 1 ]
-then
-  COWFILE="-f ${1}" # use 1st given argument
-else
-  cows=($(find /usr/share/cowsay/cows \
+then # usa la vaca que el usuario quiere
+	COWFILE="-f ${1}"
+else # ó una vaca cualquiera
+	cows=($(find /usr/share/cowsay/cows \
           -name *.cow \
-          -printf "%f\n")) # get all cows
-  cow_number=$(($RANDOM % ${#cows[@]} + 1)) # pick one randomly
-  printf "\n${cows[$cow_number]}" | \
+          -printf "%f\n")) # todas las vacas disponibles
+	cow_number=$(($RANDOM % ${#cows[@]} + 1)) # lanza los dados
+	printf "\n${cows[$cow_number]}" | \
          sed 's/.cow$/ says: \n/' | \
          lolcat --freq=0.75 --duration=4 --speed=10
-#  sleep 1
-  COWFILE=$(echo "-f ${cows[$cow_number]}" | \
+	sleep 1
+	COWFILE=$(echo "-f ${cows[$cow_number]}" | \
             sed 's/.cow$//')
 fi
 
-# keep a record in fortune.txt
-printf '\n' >> fortune.txt
+printf '\n' >> fortune.txt # respaldo
 
 fortune | tee -a fortune.txt | \
        (printf '\n' && \
        cowsay $COWFILE && printf '\n') | \
        lolcat --animate \
               --freq=0.5 --duration=4 --speed=30 \
-	      --spread=$(($RANDOM % 10 + 1)) && \
-       printf '_%.0s' {1..40} >> fortune.txt && \ # print 40 times _
+	       --spread=$(($RANDOM % 10 + 1)) && \
+       # print 40 times '_'
+       printf '_%.0s' {1..40} >> fortune.txt && \
        printf '\n' >> fortune.txt
